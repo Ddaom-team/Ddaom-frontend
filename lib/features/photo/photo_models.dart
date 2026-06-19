12950811e54dbe9399d65bff97afd006cc2734cb
DongiveupJ <kkkjs13@naver.com>
2026-06-20 01:23:21 +0900

    fix: 머지 중복필드 정리

--- lib/features/photo/photo_models.dart
+++ lib/features/photo/photo_models.dart
@@ -98,9 +98,6 @@ class PhotoInfo {
   final int likeCount;
   final String? nickname;
   final String? profileImage;
-  final String? photoSpotTitle;
-  final int? placeId;
-  final String? placeName;
 
   const PhotoInfo({
     required this.photoId,
@@ -121,9 +118,6 @@ class PhotoInfo {
     this.likeCount = 0,
     this.nickname,
     this.profileImage,
-    this.photoSpotTitle,
-    this.placeId,
-    this.placeName,
   });
 
   factory PhotoInfo.fromJson(Map<String, dynamic> json) => PhotoInfo(
@@ -160,9 +154,6 @@ class PhotoInfo {
         likeCount: (json['likeCount'] as num?)?.toInt() ?? 0,
         nickname: json['nickname'] as String?,
         profileImage: json['profileImage'] as String?,
-        photoSpotTitle: json['photoSpotTitle'] as String?,
-        placeId: (json['placeId'] as num?)?.toInt(),
-        placeName: json['placeName'] as String?,
       );
 }
 
